7385f93c73495bea08c0e2d279ebc698a337a97c
caglargulmez <furkancaglar_gulmez1997@hotmail.com>
2021-10-21 09:22:11 -0400

    until section2-25

--- app.js
+++ app.js
@@ -3,13 +3,49 @@ const app = Vue.createApp({
     data () {
         // return an data object
         return {
-            firstName: 'Caglar',
+            firstName: 'Furkan',
+            middleName: '',
             lastName: 'Gulmez',
+            url: 'https://google.com',
+            raw_url: '<p> <a href="https://google.com" target="_blank">Google</a></p> ',
+            age: 25,
+
         }
     },
     methods: {
+        incrementAge() {
+            this.age++;
+        },
+        decrementAge() {
+            this.age--;
+        },
+        updateLastName(msg, event) {
+            // if we add event modifier to html file
+            // the line below is become meaningless
+            // event.preventDefault();
+            //console.log(msg);
+            this.lastName = event.target.value;
+        },
+        updateMiddleName(event) {
+            this.middleName = event.target.value;
+        }
+    },
+    // we will use this to avoid refreshing all components 
+    // when one is changed
+    // computed: saves performance and execution times
+    computed: {
         fullName() {
-            return `${this.firstName} ${this.lastName.toUpperCase()}`
+            console.log('fullName() computed property was called')
+            
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+        },
+    },
+    watch: {
+        age(newVal, oldVal) {
+            // asyncronous tasks 
+            setTimeout( () => {
+                this.age = 30;
+            }, 3000 ) ; 
         }
     }
 
