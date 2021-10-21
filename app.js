3db9e532cf8a4cc2be52640a1d135263944fd709
caglargulmez <furkancaglar_gulmez1997@hotmail.com>
2021-10-21 09:55:19 -0400

    circle example is finished

--- app.js
+++ app.js
@@ -1,57 +1,23 @@
-const app = Vue.createApp({
-
-    data () {
-        // return an data object
+let vm = Vue.createApp({
+    data() {
         return {
-            firstName: 'Furkan',
-            middleName: '',
-            lastName: 'Gulmez',
-            url: 'https://google.com',
-            raw_url: '<p> <a href="https://google.com" target="_blank">Google</a></p> ',
-            age: 25,
-
+            isPurple: false,
+            selectedColor: '',
+            size: 150,
         }
     },
     methods: {
-        incrementAge() {
-            this.age++;
-        },
-        decrementAge() {
-            this.age--;
-        },
-        updateLastName(msg, event) {
-            // if we add event modifier to html file
-            // the line below is become meaningless
-            // event.preventDefault();
-            //console.log(msg);
-            this.lastName = event.target.value;
-        },
-        updateMiddleName(event) {
-            this.middleName = event.target.value;
-        }
+
     },
-    // we will use this to avoid refreshing all components 
-    // when one is changed
-    // computed: saves performance and execution times
     computed: {
-        fullName() {
-            console.log('fullName() computed property was called')
-            
-            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+        circle_classes() {
+            return {purple: this.isPurple}
         },
-    },
-    watch: {
-        age(newVal, oldVal) {
-            // asyncronous tasks 
-            setTimeout( () => {
-                this.age = 30;
-            }, 3000 ) ; 
+        circle_size() {
+            return [{ width: this.size + 'px', height: this.size + 'px',
+                        lineHeight: this.size + 'px'}, 
+                    { transform: 'rotate(30deg)' }
+            ]
         }
     }
-
 }).mount('#app')
-
-
-// setTimeout( () => {
-//    vm.firstName = 'CAKO'
-// }, 2000);
