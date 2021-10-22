0efed8b4c23a327b5b470b00b415c8496abd9536
caglargulmez <furkancaglar_gulmez1997@hotmail.com>
2021-10-22 03:07:32 -0400

    project1-section3 finished

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-let vm = Vue.createApp({
-  data() {
-    return {
-      people: [
-        {
-          name: 'John',
-          message: 'Hello world!'
-        },
-        {
-          name: 'Rick',
-          message: 'I like pie.'
-        },
-        {
-          name: 'Amy',
-          message: 'Skydiving is fun!'
+const vm = Vue.createApp({
+    data() {
+        return {
+            perspective: 100,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0,
         }
-      ]
+    },
+    methods: {
+        reset() {
+            this.perspective = 100;
+            this.rotateX = 0;
+            this.rotateY = 0;
+            this.rotateZ = 0;
+        },
+        copy() {
+            const el = document.createElement('textarea');
+
+            el.setAttribute('readonly', '');
+            el.style.position = 'absolute';
+            el.style.left = '-9999px'
+            el.value = `transform: ${this.box.transform}`
+
+            document.body.appendChild(el);
+            el.select();
+            // copy to clipboard action
+            document.execCommand('copy');
+            document.body.removeChild(el);
+        }
+
+        
+    },
+    computed: {
+        box() {
+            return {
+                transform: `
+                perspective(${this.perspective}px)
+                rotateX(${this.rotateX}deg)
+                rotateY(${this.rotateY}deg)
+                rotateZ(${this.rotateZ}deg)
+                `
+            }
+        }
     }
-  },
-  methods: {
-      move(){
-          const first = this.people.shift(); // remove first item and return
-          
-          this.people.push(first);
-      }
-  }
-}).mount('#app')
+}).mount('#app');
