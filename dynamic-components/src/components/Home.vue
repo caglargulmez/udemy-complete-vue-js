0a2a828bd363c946976fc46ba7998e15a1dbcbf9
Furkan Çağlar Gülmez <caglargulmez@Furkans-MacBook-Air.local>
2021-10-26 02:08:47 +0300

    dynamic components are added

---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1> Home Page </h1>   
+</template>
+
+<script>
+export default {
+    unmounted() {
+        // when switched to the about page, Home component will be destroyed
+        console.log("Home component unmounted");
+    },
+    activated() {
+        console.log("Home component activated");
+    },
+    deactivated() {
+        console.log("Home component deactivated");
+    }
+    
+}
+</script>
