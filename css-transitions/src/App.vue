a7ba93e2b14c49f07d1c9843ce262eb57929d54b
Furkan Çağlar Gülmez <caglargulmez@Furkans-MacBook-Air.local>
2021-10-26 13:01:28 +0300

    css&js transitions, animate.style

--- css-transitions/src/App.vue
+++ css-transitions/src/App.vue
@@ -7,28 +7,118 @@
     <h2 v-else key="secondary"> Another Hello! </h2>
   </transition> -->
 
-  <transition name="zoom" type="animation" appear>
+  <!-- <transition name="zoom" type="animation" appear>
     <h2 v-if="flag"> Hello </h2>
-  </transition>
+  </transition> -->
+
+  <!-- <transition
+    @before-enter="beforeEnter"
+    @enter       ="enter"
+    @after-enter ="afterEnter"
+    @before-leave="beforeLeave"
+    @leave       ="leave"
+    @after-leave ="afterLeave"
+    :css         ="true"
+    name         ="fade"
+  >
+    <h2 v-if="flag"> Hey </h2>
+  </transition> -->
+
+  <button @click="addItem"> Add </button>
+  
+  <ul>
+    <!-- enter-from-class   ="" -->
+    <!-- enter-to-class     ="" -->
+    <!-- leave-to-class     ="" -->
+    <!-- leave-from-class   ="" -->
+    <transition-group name="fade"
+      enter-active-class ="animate__animated animate__flipInX"
+      leave-active-class ="animate__animated animate__flipOutX"
+    >
+      <li 
+      v-for="(number, index) in numbers" 
+      :key="number" 
+      @click="removeItem(index)"> 
+        {{ number }} 
+      </li>
+    </transition-group>
+  </ul>
 
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
     return {
       flag: true,
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1); // random 1,100
+      const index = Math.floor(Math.random() * this.numbers.length); // 0, length-1
+      this.numbers.splice(index, 0, num);
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el,) {
+      console.log('before-enter event fired', el);
+    },
+    enter(el, /*done*/) {
+      console.log('enter event fired', el);
 
+      // first : animation
+      // second: what happens when animation is done
+      // const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+      //   duration: 1000,
+      // });
+
+      // animation.onfinish = () => {
+      //   done();
+      // }
+    },
+    afterEnter(el) {
+      console.log('after-enter event fired', el);
+    },
+    beforeLeave(el) {
+      console.log('before-leave event fired', el);
+    },
+    leave(el, /*done*/) {
+      console.log('leave event fired', el);
+      // first : animation
+      // second: what happens when animation is done
+      // const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+      //   duration: 1000,
+      // });
+
+      // animation.onfinish = () => {
+      //   done();
+      // }
+    },
+    afterLeave(el) {
+      console.log('after-leave event fired', el);
+    },
   }
 }
 </script>
 
 <style>
+.animate__flipOutX {
+  position: absolute;
+}
+
+.animate__animated {
+  animation-duration: 1.5s;
+}
+
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
+
 h2 {
   width: 400px;
   padding: 20px;
@@ -46,6 +136,14 @@ h2 {
 .fade-leave-to {
   transition: all 1s linear;
   opacity: 0;
+}
+
+.fade-move {
+  transition: all 1s linear;
+}
+
+.fade-leave-active {
+  position: absolute;
 }
 
 .zoom-enter-active {
